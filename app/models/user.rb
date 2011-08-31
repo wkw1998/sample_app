297cedcb7af92bb8f3bfc7e56afba397e6d7fd2c
Jason Wu <wkw1998@hotmail.com>
2011-08-31 21:05:30 +0800

    Added user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,14 @@
 #  updated_at         :datetime
 #  encrypted_password :string(255)
 #  salt               :string(255)
+#  admin              :boolean         default(FALSE)
 #
 
 class User < ActiveRecord::Base
 	attr_accessor :password
 	attr_accessible :name, :email, :password, :password_confirmation
+
+	has_many :microposts, :dependent => :destroy
 
 	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -28,6 +31,10 @@ class User < ActiveRecord::Base
 						 :length		=> { :within => 6..40 }
 	
 	before_save :encrypt_password
+
+	def feed
+		Micropost.where("user_id = ?", id)
+	end
 
 	def has_password?(submitted_password)
 		encrypted_password == encrypt(submitted_password)
